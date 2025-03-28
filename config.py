b6eee75d1bc33ff2165732b53e94e380f009e487
raccoon-42 <aliozkaya672@gmail.com>
2025-03-28 23:05:09 +0300

    Stop tracking config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-QDRANT_URL = "http://localhost:6333"
-# EMBEDDING_MODEL = "all-MiniLM-L6-v2"
-EMBEDDING_MODEL = "all-mpnet-base-v2" 
-EMBEDDING_MODEL2 = "nomic-ai/nomic-embed-text-v1" 
-EMBEDDING_MODEL3 = "intfloat/multilingual-e5-large-instruct" 
-
-
-COLLECTION_NAME = "academic_calendar_2025"
-OPENAI_API_KEY = "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"    
